ca69588bef89ba3ddc210e7641da3c895c05745e
Kartikeya Mishra <kartikeyavkm@gmail.com>
2025-11-05 01:14:19 +0530

    feat: init cli

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kartikm7/listit-cli
 
 go 1.24.9
+
+require github.com/urfave/cli/v3 v3.5.0
---
+++ internals/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
+
+func main() {
+	// this is super interesting, I'm from the javascript world so this is a litte freaky to me but here's the breakdown
+	// & is for pointers, so we initialize a Struct that returns it's memory address which is then used to invoke it's
+	// Run method, which expects to be called on a Pointer that holds a value of type Command
+	// Now, run takes in two arguments a Context and Arguments from the OS
+	// 1. Context: It's sole purpose is to have elegant stopping of goroutines running in the background,
+	// the Background context is ever running until the cancel() method is invoked and that can get invoked through multiple scenarios
+	// like CTRL + C, force quiting the pane, shutting down the OS, and much more.
+	(&cli.Command{}).Run(context.Background(), os.Args)
+}
