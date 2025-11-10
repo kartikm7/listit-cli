f470fd3d8b0d83bae1e93fc3767e13c9ca25b23e
Kartikeya Mishra <kartikeyavkm@gmail.com>
2025-11-11 00:25:30 +0530

    feat: added it command
    - allows to add entry to csv file (append)

--- internals/commands/it.go
+++ internals/commands/it.go
@@ -4,15 +4,22 @@ package commands
 
 import (
 	"context"
-	"fmt"
 
+	"github.com/kartikm7/listit-cli/internals/db"
 	"github.com/urfave/cli/v3"
 )
 
 func It(_ context.Context, cmd *cli.Command) error {
 	args := cmd.Args().Slice()
-	for _, v := range args {
-		fmt.Println(v)
+	record := [2]string{}
+	copy(record[:], args[:])
+	// incase status is not passed we default to Todo
+	if record[1] == "" {
+		record[1] = db.Todo
+	}
+	err := db.Add(record)
+	if err != nil {
+		panic(err)
 	}
 	return nil
 }
--- internals/db/db.go
+++ internals/db/db.go
@@ -2,22 +2,62 @@
 package db
 
 import (
+	"encoding/csv"
 	"fmt"
 	"os"
+)
+
+const (
+	Location = "/tmp/listit/db.csv"
+	Todo     = "Todo"
+	Doing    = "Doing"
+	Done     = "Done"
 )
 
 // Init func creates a file in the tmp directory by default
 // and all your data gets stored there
 func Init() error {
-	if _, err := os.Stat("/tmp/listit/db.csv"); err != nil {
+	if _, err := os.Stat(Location); err != nil {
 		fmt.Println("Database does not exist initializing it... (One time thing)")
 		// this would mean it does not exist and we must create the db
-		if err := os.MkdirAll("/tmp/listit/", 0o755); err != nil {
+		if err := os.MkdirAll("/tmp/listit/", 0o777); err != nil {
 			panic(err)
 		}
-		if err := os.WriteFile("/tmp/listit/db.csv", []byte(""), 0o644); err != nil {
+		file, err := os.Create(Location)
+		if err != nil {
 			panic(err)
 		}
+		// deferring makes it run at the end of the function call no matter what
+		defer file.Close()
+
+		writer := csv.NewWriter(file)
+		// at the end, it will write all the data written into the buffer
+		defer writer.Flush()
+
+		// these are the columns and should be added first
+		header := []string{"Task", "Status"}
+		if err := writer.Write(header); err != nil {
+			panic(err)
+		}
+	}
+	return nil
+}
+
+func Add(record [2]string) error {
+	file, err := os.OpenFile(Location, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o777)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	if err = writer.Write(record[:]); err != nil {
+		return err
+	}
+	if err = writer.Error(); err != nil {
+		return err
 	}
 	return nil
 }
